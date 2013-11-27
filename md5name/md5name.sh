590635f36c5d6bd6bb3dccfe5503133e89b22aab
Ivan Malopinsky <ivan@imsky.org>
2013-11-27 17:01:38 -0500

    improve argument support

--- md5name/md5name.sh
+++ md5name/md5name.sh
@@ -1,7 +1,16 @@
-for i in $1
+if [ $# -ne 2 ]
+then
+	echo "Usage: md5name.sh [base] [pattern]"
+	echo "Where [base] is the base directory and [pattern] is the glob pattern. The pattern is not processed recursively."
+exit
+fi
+
+dir=$(readlink -f $1)
+
+for i in $dir/$2
 do
 filename=$(basename "$i")
 extension="${filename##*.}"
-newname=$(md5sum "$filename" | cut -d ' ' -f 1 -).$extension
-mv "$filename" $newname
+newname=$(md5sum "$dir/$filename" | cut -d ' ' -f 1 -).$extension
+mv "$dir/$filename" $dir/$newname
 done
