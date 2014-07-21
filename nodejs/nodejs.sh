18a338173bc14562c7276274a0f4a426f8fda326
Ivan Malopinsky <ivan@imsky.co>
2014-07-21 16:18:34 -0400

    source nvm.sh directly

--- nodejs/nodejs.sh
+++ nodejs/nodejs.sh
@@ -1,7 +1,17 @@
+#!/usr/bin/env bash
 rm -rf ~/.nvm
+
 NVM_DIR=~/.nvm curl -L https://raw.github.com/creationix/nvm/master/install.sh | bash
-echo "export NVM_DIR=~/.nvm; source ~/.nvm/nvm.sh" >> ~/.bashrc
-source ~/.bashrc
+
+if grep NVM_DIR ~/.bashrc
+then
+	echo "NVM_DIR already set."
+else
+	printf "export NVM_DIR=~/.nvm;\n. ~/.nvm/nvm.sh" >> ~/.bashrc
+fi
+
+. ~/.nvm/nvm.sh
+
 nvm install 0.10
 nvm alias default 0.10
 npm install -g bower grunt node-gyp
