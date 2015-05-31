7170b12fd580529ccee33631e344a23803683494
Ivan Malopinsky <ivan@imsky.co>
2015-05-31 15:28:54 -0400

    update node.js script to allow any nvm_dir, remove preinstalled deps

--- nodejs/nodejs.sh
+++ nodejs/nodejs.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.nvm
-NVM_DIR=~/.nvm curl -L https://raw.github.com/creationix/nvm/master/install.sh | bash
+NVM_DIR=${1:-~/.nvm}
+
+rm -rf $NVM_DIR
+NVM_DIR=$NVM_DIR curl -L  https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 if grep NVM_DIR ~/.bashrc
 then
-	echo "NVM_DIR already set."
+ echo "NVM_DIR already set."
 else
-	printf "export NVM_DIR=~/.nvm;\n. ~/.nvm/nvm.sh" >> ~/.bashrc
+ printf "export NVM_DIR=$NVM_DIR;\n. $NVM_DIR/nvm.sh" >> ~/.bashrc
 fi
 
-. ~/.nvm/nvm.sh
-nvm install 0.10
-nvm alias default 0.10
-npm install -g bower grunt-cli node-gyp gulp
+. $NVM_DIR/nvm.sh
+nvm install 0.12
+nvm alias default 0.12
